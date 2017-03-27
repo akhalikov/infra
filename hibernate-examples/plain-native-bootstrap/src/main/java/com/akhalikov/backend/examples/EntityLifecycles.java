3aa1b03a28c7352b939ae6d5c2bb52abf1082601
Artur Khalikov <akhalikoff@gmail.com>
2017-03-28 01:26:22 +0300

    entity lifecycles example

--- hibernate-examples/plain-native-bootstrap/src/main/java/com/akhalikov/backend/examples/EntityLifecycles.java
+++ hibernate-examples/plain-native-bootstrap/src/main/java/com/akhalikov/backend/examples/EntityLifecycles.java
@@ -4,7 +4,6 @@ import com.akhalikov.backend.users.User;
 import com.akhalikov.backend.utils.PropertiesFactory;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 
 import java.util.Properties;
 
