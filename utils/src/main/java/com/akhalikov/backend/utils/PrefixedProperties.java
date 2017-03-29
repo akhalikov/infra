70fd037cdf0b9d62a9d2d88edb12e6eef7371e34
Artur Khalikov <akhalikoff@gmail.com>
2017-03-29 23:59:45 +0300

    backend utils

---
+++ utils/src/main/java/com/akhalikov/backend/utils/PrefixedProperties.java
@@ -0,0 +1,25 @@
+package com.akhalikov.backend.utils;
+
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class PrefixedProperties extends Properties {
+
+  @SuppressWarnings("unchecked")
+  public PrefixedProperties(Properties properties, String prefix){
+    if (properties == null){
+      return;
+    }
+
+    Enumeration<String> en = (Enumeration<String>) properties.propertyNames();
+    while (en.hasMoreElements()){
+      String propName = en.nextElement();
+      String propValue = properties.getProperty(propName);
+
+      if(propName.startsWith(prefix)){
+        String key = propName.substring(prefix.length());
+        setProperty(key, propValue);
+      }
+    }
+  }
+}
